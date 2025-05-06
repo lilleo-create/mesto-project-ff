c9142d71933975958c7781b992015bf1d9ccc0e9
leonid <LaggerFint@yandex.ru>
2025-05-06 14:54:08 +0300

    реализована отрисовка и удаление карточек

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,7 +1,7 @@
 const initialCards = [
     {
       name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      link: "https://as2.ftcdn.net/v2/jpg/05/43/61/87/1000_F_543618781_3S3Pjuufrn8H6E15LO5ruuIxBjMmPd9r.jpg",
     },
     {
       name: "Челябинская область",
@@ -13,11 +13,11 @@ const initialCards = [
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      link: "https://images.unsplash.com/photo-1708744609316-5c37132f60db?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Москва",
+      link: "https://images.unsplash.com/photo-1523263983652-bb7473e297ad?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Байкал",
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
+const placesList = document.querySelector('.places__list');
+
+
 
 // @todo: DOM узлы
+// Пока не используются
+
 
 // @todo: Функция создания карточки
 
+function createCard (cardData, handleDelete) {
+  const cardElement = cardTemplate.cloneNode(true).querySelector('.card')
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener('click', () => handleDelete(cardElement))
+
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function handleDeleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, handleDeleteCard);
+  placesList.appendChild(card);
+});
